b9c6c28e5a32978bfca15179c472dd788022e652
DarrenLe20 <89809192+DarrenLe20@users.noreply.github.com>
2023-02-15 11:57:47 -0500

    final

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -16,13 +16,10 @@ const App = props => {
           <Routes>
             {/* a route for the home page */}
             <Route path="/" element={<Home />} />
-
             {/* a route to see a list of all messages */}
             <Route path="/messages" element={<Messages />} />
-
             {/* a route to see the about page */}
             <Route path="/about" element={<About />} />
-
             {/* a route for just a single message, where the id of the desired message is passed as a parameter */}
             <Route
               path="/messages/:messageId"
